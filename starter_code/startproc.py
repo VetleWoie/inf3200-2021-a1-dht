9c771968196868e4cb1c3ec7c2c285095782e7ce
Vetle Hofsoy-Woie <vho023@uit.no>
2021-09-23 14:42:03 +0200

    starting script for local

--- starter_code/startproc.py
+++ starter_code/startproc.py
@@ -1,6 +1,14 @@
-import subprocess
+from subprocess import Popen, PIPE 
+from signal import SIGINT, signal, SIGTERM
 import sys
 
+PROCS = []
+
+def signal_handle(signum, frame):
+    print("Terminating processes")
+    for p in PROCS:
+        p.send_signal(SIGTERM)
+        exit()
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print(f"Usage: {sys.argv[0]} [number of nodes] {{starter port}}")
@@ -11,15 +19,45 @@ if __name__ == '__main__':
     else:
         port = 8000
     
+    signal(SIGTERM, signal_handle)
+    signal(SIGINT, signal_handle)
+
+    num_nodes = int(sys.argv[1])
+
+    commands = []
+    
     ip = "127.0.0.1:"
-    command = "python3 dummynode.py -p "
     ports = []
+    for i in range(port, port + num_nodes):
+        ports.append(ip + str(i))
 
-    tmp = port
-    for i in range(sys.argv[1]):
-        ports[i] = ip + str(tmp)
-        tmp += 1
+    command = ["python3", "dummynode.py", "-p"]
+    for i in range(num_nodes):
+        tmp = ports[0]
+        ports[0] = ports[i]
+        ports[i] = tmp
+        commands.append(command + [ports[0].split(":")[1]] + ports)
 
-    nodestring = str(ports).replace(',', '').replace('[','').replace(']','')
-    
-    
+    for command in commands:
+        print("Running command: ", command)
+        PROCS.append(
+            Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            )
+
+    while(True):
+        if len(PROCS) == 0:
+            break
+        for p in PROCS:
+            if p.poll() is None:
+                pass
+            else:
+                #Check wether proces terminated by error
+                output, err = p.communicate()
+                print("Error: One proces exited with the following error:\n\n")
+                print(err.decode())
+                signal_handle(0,0)
+                
+        
+
+
+
